695652fe58a5958e0f9fc5b63a4b901c0c78f310
Alex <sanchev_temp@inbox.ru>
2017-08-18 14:28:16 +0300

    Added logging

--- src/main/java/com/sanchev/App.java
+++ src/main/java/com/sanchev/App.java
@@ -1,12 +1,18 @@
 package com.sanchev;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.GregorianCalendar;
 
 public class App {
+    private static final Logger LOGGER = LogManager.getLogger(App.class.getName());
 
     public static void main(String[] args) {
+        LOGGER.info("App started");
         Greeter greeter = new Greeter();
         System.out.println(greeter.getMessage(greeter.getDayPart(new GregorianCalendar())));
+        LOGGER.info("App finished");
     }
 
 }
--- src/main/java/com/sanchev/Greeter.java
+++ src/main/java/com/sanchev/Greeter.java
@@ -1,27 +1,41 @@
 package com.sanchev;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Greeter {
+class Greeter {
+    private static final Logger LOGGER = LogManager.getLogger(Greeter.class.getName());
 
     enum DayPart {MORNING, DAY, EVENING, NIGHT}
 
     DayPart getDayPart(Calendar calendar) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        LOGGER.info(String.format("Time: %s", simpleDateFormat.format(calendar.getTime())));
         int hh = calendar.get(Calendar.HOUR_OF_DAY);
+        DayPart dayPart;
         if ((hh >= 6) && (hh < 9))
-            return DayPart.MORNING;
+            dayPart = DayPart.MORNING;
         else if ((hh >= 9) && (hh < 19))
-            return DayPart.DAY;
+            dayPart = DayPart.DAY;
         else if ((hh >= 19) && (hh < 23))
-            return DayPart.EVENING;
+            dayPart = DayPart.EVENING;
         else
-            return DayPart.NIGHT;
+            dayPart = DayPart.NIGHT;
+        LOGGER.info(String.format("Part of day: %s", dayPart));
+        return dayPart;
     }
 
     String getMessage(DayPart dayPart) {
+        LOGGER.info(String.format("Locale: %s", Locale.getDefault()));
         ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
-        return resourceBundle.getString(dayPart.toString());
+        String message = resourceBundle.getString(dayPart.toString());
+        LOGGER.info(String.format("Message: %s", message));
+        return message;
     }
 
 }
